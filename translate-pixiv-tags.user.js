eb9a9b9e346bc1a58d6440cf070212fcc68ef559
7nik <kfiiranet@gmail.com>
2019-02-24 18:23:54 +0200

    twitter: align the artist tag position in tweet replaies

--- translate-pixiv-tags.user.js
+++ translate-pixiv-tags.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Translate Pixiv Tags
 // @author       evazion
-// @version      20190222195346
+// @version      20190224182246
 // @description  Translates tags on Pixiv, Nijie, NicoSeiga, Tinami, and BCY to Danbooru tags.
 // @homepageURL  https://github.com/evazion/translate-pixiv-tags
 // @supportURL   https://github.com/evazion/translate-pixiv-tags/issues
@@ -372,6 +372,11 @@ $("head").append(`
     }
     #ex-twitter .permalink-header .ex-artist-tag {
         grid-row: 2;
+    }
+
+    #ex-twitter .ReplyingToContextBelowAuthor .ex-artist-tag {
+        display: inline-block;
+        margin-left: 5px;
     }
 
     /* Render the Danbooru artist tag on the same line as the Twitter username. */
