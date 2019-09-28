c25e7855898423f79468106d28c704f6a50028f7
7nik <kfiiranet@gmail.com>
2019-09-28 23:06:15 +0300

    no array re-creating

--- translate-pixiv-tags.user.js
+++ translate-pixiv-tags.user.js
@@ -411,7 +411,7 @@ async function translateArtistByURL(element, profileUrl, options) {
     artists
         // fix of #18: for some unsupported domains, Danbooru returns false-positive results
         .filter(({urls}) => urls
-            .reduce((urls, {url, normalized_url}) => [...urls, url, normalized_url], []) // for old Firefox
+            .reduce((urls, {url, normalized_url}) => { urls.push(url, normalized_url); return urls; }, []) // for old Firefox
             .map(url => new URL(url.replace(/\/$/,"").toLowerCase()))
             .some(aUrl => (pUrl.host==aUrl.host && pUrl.pathname==aUrl.pathname && pUrl.search==aUrl.search)))
         .map(artist => addDanbooruArtist($(element), artist, options));
