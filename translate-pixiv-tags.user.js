8a6fd18428f173d4b4568deaa2e929976495d1a6
7nik <kfiiranet@gmail.com>
2019-10-05 00:56:54 +0300

    code styling: no-restricted-properties

--- translate-pixiv-tags.user.js
+++ translate-pixiv-tags.user.js
@@ -940,7 +940,7 @@ function timeToAgo(time) {
 function formatBytes(bytes) {
     const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
     const i = Math.floor(Math.log(bytes) / Math.log(1024));
-    return parseFloat((bytes / Math.pow(1024, i)).toFixed(2)) + " " + sizes[i];
+    return parseFloat((bytes / (1024 ** i)).toFixed(2)) + " " + sizes[i];
 }
 
 function buildPostPreview(post) {
