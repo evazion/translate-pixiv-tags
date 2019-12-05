be7da642fb1d298da2a20893961367f0aec55960
7nik <kfiiranet@gmail.com>
2019-12-05 17:17:12 +0200

    version increment

--- translate-pixiv-tags.user.js
+++ translate-pixiv-tags.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Translate Pixiv Tags
 // @author       evazion
-// @version      20191202110946
+// @version      20191205171746
 // @description  Translates tags on Pixiv, Nijie, NicoSeiga, Tinami, and BCY to Danbooru tags.
 // @homepageURL  https://github.com/evazion/translate-pixiv-tags
 // @supportURL   https://github.com/evazion/translate-pixiv-tags/issues
