0721219720cf540d5909d6cc12c21c89004c0907
7nik <kfiiranet@gmail.com>
2020-01-09 09:46:31 +0200

    Add Safebooru subdomain

--- translate-pixiv-tags.user.js
+++ translate-pixiv-tags.user.js
@@ -56,6 +56,7 @@ const SETTINGS = {
                 "https://kagamihara.donmai.us": "Kagamihara",
                 "https://saitou.donmai.us":     "Saitou",
                 "https://shima.donmai.us":      "Shima",
+                "https://safebooru.donmai.us/": "Safebooru",
             },
         }, {
             name: "cache_lifetime",
