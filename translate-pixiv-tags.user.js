a5d87b1c04e64c7613abce26ce232599a0b1dca5
7nik <kifiranet@gmail.com>
2022-01-31 00:13:52 +0200

    fix settings menu bg

--- translate-pixiv-tags.user.js
+++ translate-pixiv-tags.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Translate Pixiv Tags
 // @author       evazion
-// @version      20220130215646
+// @version      20220131001346
 // @description  Translates tags on Pixiv, Nijie, NicoSeiga, Tinami, and BCY to Danbooru tags.
 // @homepageURL  https://github.com/evazion/translate-pixiv-tags
 // @supportURL   https://github.com/evazion/translate-pixiv-tags/issues
@@ -1627,8 +1627,8 @@ function showSettings () {
     $settings.find(".cancel").click(closeSettings);
     $(document).keydown(closeSettingsOnEscape);
 
-    const { qtipClass } = chooseBackgroundColorScheme($("#ex-qtips"));
-    $settings.addClass(qtipClass);
+    const { theme } = chooseBackgroundColorScheme($("#ex-qtips"));
+    $settings.addClass(`qtip-${theme}`);
 
     attachShadow($shadowContainer, $settings, styles);
 }
